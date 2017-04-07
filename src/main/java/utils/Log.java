b20749fb0536889bace19a2abe2daa17f2387727
spencerluo <spencerluo@s3graphics.com>
2017-04-07 13:46:43 +0800

    changlist 1

--- src/main/java/utils/Log.java
+++ src/main/java/utils/Log.java
@@ -5,18 +5,10 @@ import org.apache.logging.log4j.Logger;
 
 public class Log {
 
-	static{
-		String date = Actions.getDate();
-		System.setProperty("myconfig.accout", date+".log");
-	}
+
 	private static Logger log = LogManager.getLogger(Log.class);
 	
-//	public static Logger log;
-//	public static void init() throws Exception, IOException{
-//		ConfigurationSource source= new ConfigurationSource(new FileInputStream("D:\\eclipse-jee-mars-1-win32-x86_64\\workspace\\miniblog\\document\\log4j2.xml"));
-//		Configurator.initialize(null,source);
-//		log = LogManager.getLogger(Log.class);
-//	}
+
 
 	public static void info(String data) {
 		log.info(data);
