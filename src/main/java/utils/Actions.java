aaf901fe71af38e48a259b4c01dc7b93464e840b
spencerluo <spencerluo@s3graphics.com>
2017-06-23 16:04:59 +0800

    add

--- src/main/java/utils/Actions.java
+++ src/main/java/utils/Actions.java
@@ -218,7 +218,11 @@ public class Actions {
 	}
 
 	public static void waitElement(String pageName, String objectName) throws Exception {
-		new WebDriverWait(driver, 10)
-				.until(ExpectedConditions.presenceOfElementLocated(Locator.getLocator(pageName, objectName)));
+		try {
+			new WebDriverWait(driver, 10)
+					.until(ExpectedConditions.presenceOfElementLocated(Locator.getLocator(pageName, objectName)));
+		} catch (Exception e) {
+			throw new Exception("[" + objectName + "] in [" + pageName + "] don't appear at 10s");
+		}
 	}
 }
